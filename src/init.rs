5a05fa8ee3b415ac5f85afd06fd109701aeca1db
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-07-26 04:38:35 +0100

    Added locked initialization helpers

--- src/init.rs
+++ src/init.rs
@@ -4,10 +4,10 @@ Utilities for initializing Lean's runtime
 use parking_lot::Mutex;
 
 /// A convenience mutex, since [`lean_initialize_runtime_module`] and [`lean_initialize`] are *not* thread-safe.
-/// 
+///
 /// It is convention to hold this when initializing Lean's runtime, or Lean modules, to make sure only one thread at a time is doing so.
 /// This is used in this library to safely implement tests, but it is the user's responsibility to uphold thread-safety when using this API.
-/// 
+///
 /// # Examples
 /// ```rust
 /// # use lean_sys::*;
@@ -20,6 +20,37 @@ use parking_lot::Mutex;
 /// assert!(!lean_is_scalar(big_nat));
 /// ```
 pub static LEAN_INIT_MUTEX: Mutex<()> = Mutex::new(());
+
+/// A helper function to call [`lean_initialize_runtime_module`] while holding the [`LEAN_INIT_MUTEX`].
+/// 
+/// This is equivalent to writing
+/// ```rust
+/// # use lean_sys::*;
+/// unsafe {
+///     let guard = LEAN_INIT_MUTEX.lock();
+///     lean_initialize_runtime_module();
+/// }
+/// ```
+//TODO: is this safe?
+pub unsafe fn lean_initialize_runtime_module_locked() {
+    let _guard = LEAN_INIT_MUTEX.lock();
+    lean_initialize_runtime_module();
+}
+
+/// A helper function to call [`lean_initialize`] while holding the [`LEAN_INIT_MUTEX`]./// 
+/// This is equivalent to writing
+/// ```rust
+/// # use lean_sys::*;
+/// unsafe {
+///     let guard = LEAN_INIT_MUTEX.lock();
+///     lean_initialize();
+/// }
+/// ```
+//TODO: is this safe?
+pub unsafe fn lean_initialize_locked() {
+    let _guard = LEAN_INIT_MUTEX.lock();
+    lean_initialize();
+}
 
 #[link(name = "leanshared")]
 extern "C" {
